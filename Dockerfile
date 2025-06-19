910c57f5496ba31af2b8161d2e3962158f6355f4
alessandro pellicone <alessandro.pellicone@liceobanfi.eu>
2025-06-19 10:42:34 +0200

    add changes in dockerfile, run.sh

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,10 @@
 # Use the official lightweight Python image
 FROM python:3.12-slim
+#install all python libraries
+RUN pip install matplotlib
+#set the working directory
+WORKDIR /app
+#copy our application
+COPY run.sh /app/run.sh
+#start the bot application
+CMD ["/app/run.sh"]
---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+# Infinite loop with a sleep of 20 minutes
+#/usr/local/bin/python /app/CiscoBanfi.py
+echo "sto partendo" > /app/test.txt
+while true; do
+  # Inserisci qui il tuo codice
+  sleep 20
+  echo "sono vivo" >> /app/test.txt
+  date +"%H:%M:%S" >> /app/test.txt
+done
+echo "ho finito" >> /app/test.txt
