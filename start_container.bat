08bdb825d6c33479af63589033d32f06b4ad1669
Michele Fontanella <mfontane@cisco.com>
2025-06-29 09:51:04 +0200

    correct container name

--- start_container.bat
+++ start_container.bat
@@ -5,10 +5,10 @@ HOST="/data/host"  # write here the path of mounted volume
 echo "docker run -d \
              --restart always \
              -v $HOST:/data/host \
-             --name scobanfi_bot ciscobanfi_image"             
+             --name ciscobanfi_bot ciscobanfi_image"             
              
 docker run -d \
              --restart always \
              -v $HOST:/data/host \
-             --name scobanfi_bot ciscobanfi_image
+             --name ciscobanfi_bot ciscobanfi_image
 
